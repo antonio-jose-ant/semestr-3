0583fabf40f8fde0f185ab82384bb8adbe712417
antonio-jose-ant <52549121+antonio-jose-ant@users.noreply.github.com>
2021-09-14 07:29:29 -0500

    Update pila.java

--- pila.java
+++ pila.java
@@ -3,37 +3,42 @@
 import java.net.Socket;
 import java.util.Scanner;
 
-public class pila {
+public class pila2 {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        int cont=0,opt=0;
        char [] pila = new char[26];
        do{
-        System.out.print("1= agregar\n2= Mostrar \n3= Eliminar\n4= Salir\n");
+        System.out.print("1= agregar\n2= Eliminar\n3= Mostrar\n 4= Salir\n");
         switch (opt = sc.nextInt()) {
             case 1:
                 if(cont == pila.length){
                     System.out.println("pila llena....");
                 }else{
-                    System.out.println("agreaga la letra " + (cont+1));
-                    pila[cont]= sc.next().charAt(0);
-                    System.out.println("elemento guardadoen la posicion: "+ (cont+1));       
-                    cont++;
+                    if(cont<pila.length){
+                        for(int i=cont;i<26;i++){
+                            System.out.println("agreaga la letra " + (cont+1));
+                            pila[cont]= sc.next().charAt(0);
+                            System.out.println("elemento guardadoen la posicion: "+ (cont+1));       
+                            cont++;
+                        }
+                    }
+                    
                 }
             break;
             case 2:
+            if(cont > 0){
+                System.out.println("dato "+ cont  +" eliminado....");
+                cont--;
+            }else{
+                System.out.println("pila vacia....");
+            }
+            break;
+            case 3:
                 if(cont >0){
                     for (int i= cont-1; i>=0;i--){
                         System.out.println(" " + pila[i]);
                     }
-                }else{
-                    System.out.println("pila vacia....");
-                }
-            break;
-            case 3:
-                if(cont > 0){
-                    System.out.println("dato "+ cont  +" eliminado....");
-                    cont--;
                 }else{
                     System.out.println("pila vacia....");
                 }
