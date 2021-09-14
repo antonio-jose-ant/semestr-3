f21113905fc033fd4050355b36ebaaa9855bc8a6
antonio-jose-ant <52549121+antonio-jose-ant@users.noreply.github.com>
2021-09-14 07:29:46 -0500

    Update pila.java

--- pila.java
+++ pila.java
@@ -3,7 +3,7 @@
 import java.net.Socket;
 import java.util.Scanner;
 
-public class pila2 {
+public class pila {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        int cont=0,opt=0;
